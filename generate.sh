c2c73738fde86203075c338b4bd19bd5fa519335
ww2 <ww2@andrew.cmu.edu>
2011-06-30 17:39:20 -0400

    Changed generate.sh to record total graphs generated and also number of malformed graphs generated

--- generate.sh
+++ generate.sh
@@ -3,9 +3,19 @@ if [ -d "$1" ]
 then
   l=`find $1 -type f -printf "%p\n"`
   x=1
+  y=0
+  z=0
   for f in ${l}
   do
-    cat ${f} | java SpecRecon ${x} 
-    x=$(($x+1))
+    cat ${f} | java Reconstruct ${x} 
+    if [ $? -eq 1 ]
+    then
+      y=$(($y+1))
+    else
+      x=$(($x+1))
+    fi
+    z=$(($z+1))
   done
+  print "Total number: $z\n"
+  print "Malformed: $y\n"
 fi
